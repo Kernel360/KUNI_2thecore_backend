29e6da1d7c0f43387f3c1c21b3033f2ea14650e3
yeojin-g <84260219+yeojin-g@users.noreply.github.com>
2025-07-08 16:35:04 +0900

    feat: car 기본 파일 구조 설정 및 Entity, Respository, Controller 파일 추가

---
+++ back/src/main/java/com/example/_thecore_back/rest/car/db/CarStatus.java
@@ -0,0 +1,7 @@
+package com.example._thecore_back.rest.car.db;
+
+public enum CarStatus { // 차량 상태를 나타내는그럼 ENUM
+    IN_USE,        // 운행 중
+    IDLE,          // 대기 중
+    MAINTENANCE    // 수리 중
+}
