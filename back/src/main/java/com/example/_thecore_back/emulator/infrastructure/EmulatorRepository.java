668aaf1d688b5085a8cd906ae0c556d9fd801db5
bomi0320 <bomi0320@naver.com>
2025-07-21 12:01:16 +0900

    chore: 필요없는 파일 삭제

--- back/src/main/java/com/example/_thecore_back/emulator/infrastructure/EmulatorRepository.java
+++ back/src/main/java/com/example/_thecore_back/emulator/infrastructure/EmulatorRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface EmulatorRepository extends JpaRepository<EmulatorEntity, Long> {
     // TODO: car_number -> device_id 임시 변경. 추후 car테이블 머지 후 수정 필요
     Optional<EmulatorEntity> findByDeviceId(String deviceId);
+
 }
