e06b14586b6728dfc505244e3d729c39c129dc09
yeojin-g <84260219+yeojin-g@users.noreply.github.com>
2025-07-24 18:17:08 +0900

    Feat: EmulatorStatus enum에 displayName 필드 추가 및 문자열 ↔ enum 변환 메서드 구현

---
+++ back/emulator-server/src/main/java/com/example/emulatorserver/device/domain/emulator/EmulatorStatus.java
@@ -0,0 +1,25 @@
+package com.example.emulatorserver.device.domain.emulator;
+
+import lombok.Getter;
+
+@Getter
+public enum EmulatorStatus {
+    ON("ON"),
+    OFF("OFF");
+
+    private final String displayName;
+
+    EmulatorStatus(String displayName) {
+        this.displayName = displayName;
+    }
+
+    // StringToStatus
+    public static EmulatorStatus getEmulatorStatus(String displayName) {
+        for (EmulatorStatus emulatorStatus : EmulatorStatus.values()) {
+            if (emulatorStatus.getDisplayName().equals(displayName)) {
+                return emulatorStatus;
+            }
+        }
+        throw new IllegalArgumentException("존재하지 않는 status: " + displayName);
+    }
+}
