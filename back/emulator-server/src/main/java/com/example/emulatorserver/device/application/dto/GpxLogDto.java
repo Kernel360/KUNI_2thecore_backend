d8490fb1f9598e22544cea82616741c8c7ac233b
yeojin-g <84260219+yeojin-g@users.noreply.github.com>
2025-07-31 12:19:17 +0900

    feat: GPX 로그 JSON 변환 로직 삭제

--- back/emulator-server/src/main/java/com/example/emulatorserver/device/application/dto/GpxLogDto.java
+++ back/emulator-server/src/main/java/com/example/emulatorserver/device/application/dto/GpxLogDto.java
@@ -1,49 +1,16 @@
 package com.example.emulatorserver.device.application.dto;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.List;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
+import lombok.ToString;
 
 @Builder
 @Getter
 @Setter
+@ToString
 public class GpxLogDto {
     private String timeStamp;
-    private double latitude; // 위도
-    private double longitude; // 경도
-
-    // Json화
-    public static String listToJson(List<List<String>> bufferlist) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        List<GpxLogDto> gpxList = new java.util.ArrayList<>();
-
-        // 정규표현식
-        Pattern pattern = Pattern.compile("lat=\"(.*?)\"\\s+lon=\"(.*?)\"");
-
-        for (List<String> log : bufferlist) {
-            String timestamp = log.get(0);
-            String trkpt = log.get(1);
-
-            Matcher matcher = pattern.matcher(trkpt);
-            if (matcher.find()) {
-                String lat = matcher.group(1);
-                String lon = matcher.group(2);
-
-                GpxLogDto dto = GpxLogDto.builder()
-                        .timeStamp(timestamp)
-                        .latitude(Double.parseDouble(lat))
-                        .longitude(Double.parseDouble(lon))
-                        .build();
-                gpxList.add(dto);
-            }
-        }
-
-        return mapper.writeValueAsString(gpxList);
-    }
+    private String latitude; // 위도
+    private String longitude; // 경도
 }
