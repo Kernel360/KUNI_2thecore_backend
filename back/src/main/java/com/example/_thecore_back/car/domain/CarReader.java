6dea70ad5b038b4cfcbd9a666150cdc9b717f94b
luckyisjelly <133952441+luckyisjelly@users.noreply.github.com>
2025-08-04 11:36:38 +0900

    feat: List import 구문 추가

--- back/src/main/java/com/example/_thecore_back/car/domain/CarReader.java
+++ back/src/main/java/com/example/_thecore_back/car/domain/CarReader.java
@@ -3,6 +3,7 @@ package com.example._thecore_back.car.domain;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
