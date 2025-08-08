74a6130d2060a8b8d846c058bc72619c90370d49
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2025-08-08 10:52:17 +0900

    feat: emulator-server 모듈 삭제 및 관련 설정 제거 (#99)

--- back/emulator-server/src/main/java/com/example/emulatorserver/device/exception/car/CarNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.emulatorserver.device.exception.car;
-
-public class CarNotFoundException extends RuntimeException {
-
-    public CarNotFoundException(CarErrorCode carErrorCode, Object... args) {
-        super(carErrorCode.format(args));
-    }
-
-}
