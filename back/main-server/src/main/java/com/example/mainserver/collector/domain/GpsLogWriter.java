53196142de71edfc5e0aa3009ef14a795bf31727
jongyo0N <jongyoon5960@kookmin.ac.kr>
2025-08-12 11:09:11 +0900

    emulator-server와 common만 남겨놓았습니다

--- back/main-server/src/main/java/com/example/mainserver/collector/domain/GpsLogWriter.java
+++
@@ -1,9 +0,0 @@
-package com.example.mainserver.collector.domain;
-
-import java.util.List;
-
-public interface GpsLogWriter {
-
-    void saveAll(List<GpsLogEntity> logs);
-
-}
--- back/main-server/src/main/java/com/example/mainserver/drivelog/domain/DriveLogRepositoryCustom.java
+++
@@ -1,8 +0,0 @@
-package com.example.mainserver.drivelog.domain;
-
-import java.time.LocalDateTime;
-import java.util.List;
-
-public interface DriveLogRepositoryCustom {
-    List<DriveLog> searchByConditions(Long carId, LocalDateTime startTime, LocalDateTime endTime);
-}
