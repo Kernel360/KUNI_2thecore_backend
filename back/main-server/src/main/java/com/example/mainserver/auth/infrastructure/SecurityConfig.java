c934c429c57eb38fe777fb8c4464fb66575bad9e
이총명 <dlchdaud04@naver.com>
2025-08-16 16:55:06 +0900

    chore: cors 에러 수정

--- back/main-server/src/main/java/com/example/mainserver/auth/infrastructure/SecurityConfig.java
+++ back/main-server/src/main/java/com/example/mainserver/auth/infrastructure/SecurityConfig.java
@@ -69,9 +69,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
         configuration.setExposedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
