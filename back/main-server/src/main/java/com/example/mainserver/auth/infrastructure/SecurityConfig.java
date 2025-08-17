5e9132a433902bc4068f382b1a0942a68437526f
이총명 <dlchdaud04@naver.com>
2025-08-17 23:21:39 +0900

    chore: cors가 로컬호스트 3006까지 허용하게 변경

--- back/main-server/src/main/java/com/example/mainserver/auth/infrastructure/SecurityConfig.java
+++ back/main-server/src/main/java/com/example/mainserver/auth/infrastructure/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://2thecore20250809.s3-website.ap-northeast-2.amazonaws.com"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "http://localhost:3003", "http://localhost:3004", "http://localhost:3005", "http://localhost:3006", "http://2thecore20250809.s3-website.ap-northeast-2.amazonaws.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
