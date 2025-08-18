5c93ef62a3a78a34b7564d16b1894977adc12462
jongyo0N <jongyoon5960@kookmin.ac.kr>
2025-08-18 15:37:42 +0900

    .

--- back/main-server/src/main/java/com/example/mainserver/car/domain/CarWriter.java
+++ back/main-server/src/main/java/com/example/mainserver/car/domain/CarWriter.java
@@ -3,7 +3,7 @@ package com.example.mainserver.car.domain;
 import com.example.common.domain.car.CarEntity;
 
 public interface CarWriter {
-    CarEntity save(CarEntity carEntity);
 
+    CarEntity save(CarEntity carEntity);
     void delete(CarEntity carEntity);
 }
