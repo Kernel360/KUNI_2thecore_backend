b0ca10be5ab5b5e9bb06eaa621161c0657fc1190
bomi0320 <bomi0320@naver.com>
2025-08-19 16:17:27 +0900

    차량 생성 시 유효성 검사 추가(차량번호, 차량연식, 총주행거리)

--- back/common/src/main/java/com/example/common/dto/CarRequestDto.java
+++ back/common/src/main/java/com/example/common/dto/CarRequestDto.java
@@ -1,7 +1,9 @@
 package com.example.common.dto;
 
 import com.example.common.group.CreateGroup;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Getter
@@ -17,6 +19,7 @@ public class CarRequestDto {
     @NotBlank(groups = CreateGroup.class)
     private String model;
 
+    @Min(value = 0, message = "차량 연식은 0 이상이어야 합니다.", groups = CreateGroup.class)
     private Integer carYear;
 
     private String status;
@@ -25,9 +28,11 @@ public class CarRequestDto {
     private String carType;
 
     @NotBlank(groups = CreateGroup.class)
+    @Pattern(regexp = "\\d{2,3}[가-힣]{1}\\d{4}", message = "차량 번호 형식이 올바르지 않습니다.", groups = CreateGroup.class)
     private String carNumber;
 
     // @NotNull(groups = CreateGroup.class)
+    @Min(value = 0, message = "주행 거리는 0 이상이어야 합니다.", groups = CreateGroup.class)
     private Double sumDist = 0.00;
 
     private String lastLatitude;
