a7b1d03c8a839dad8dae2de2a76a973da5a144f8
Chyopriushy <mmkanggg@gmail.com>
2025-08-20 16:24:51 +0900

    feat: driveLogResponseDto의 start end time을 LocalDateTime으로 변경

--- back/main-server/src/main/java/com/example/mainserver/drivelog/dto/DriveLogFilterResponseDto.java
+++ back/main-server/src/main/java/com/example/mainserver/drivelog/dto/DriveLogFilterResponseDto.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 
 @Getter
@@ -22,11 +23,11 @@ public class DriveLogFilterResponseDto {
     private String model;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssXXX")
-    private OffsetDateTime startTime;
+    private LocalDateTime startTime;
 
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssXXX")
-    private OffsetDateTime endTime;
+    private LocalDateTime endTime;
 
 
     private String startPoint;
