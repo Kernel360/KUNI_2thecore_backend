d1dff3412e40383071befb21560323a61cc5db28
jongyo0N <jongyoon5960@kookmin.ac.kr>
2025-08-21 15:56:46 +0900

    .

--- back/hub-server/src/main/java/hub/application/ConsumerService.java
+++ back/hub-server/src/main/java/hub/application/ConsumerService.java
@@ -44,7 +44,6 @@ public class ConsumerService {
                 .sorted(Comparator.comparing(GpsLogDto.Gps::getTimestamp))
                 .toList();
 
-        lastPositionUpdator.scheduleEverySecond(carEntity.getCarNumber(), sortedGpsList);
 
         var entities = sortedGpsList.stream()
                 .map(g -> new GpsLogEntity(gpsLogDto.getCarNumber(), g.getLatitude(), g.getLongitude(), g.getTimestamp()))
@@ -52,6 +51,7 @@ public class ConsumerService {
 
         gpsLogRepository.saveAll(entities);
 
+        lastPositionUpdator.scheduleEverySecond(carEntity.getCarNumber(), sortedGpsList);
 //        //정렬된 리스트 차례로 저장
 //        for (GpsLogDto.Gps gps : sortedGpsList) {
 //            try {
