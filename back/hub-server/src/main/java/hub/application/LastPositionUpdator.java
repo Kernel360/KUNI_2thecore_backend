2c43bcc57b81be56485606602b41330c25ce752d
jongyo0N <jongyoon5960@kookmin.ac.kr>
2025-08-21 16:33:29 +0900

    Bug: chunk 읽기 시 동시성 문제 발생 3 chunk 5

--- back/hub-server/src/main/java/hub/application/LastPositionUpdator.java
+++ back/hub-server/src/main/java/hub/application/LastPositionUpdator.java
@@ -20,7 +20,7 @@ public class LastPositionUpdator {
 
     public void scheduleEverySecond(String carNumber, List<GpsLogDto.Gps> sorted) {
 
-        final int CHUNK = 3;
+        final int CHUNK = 5;
 
         var latestPerChunk = new ArrayList<GpsLogDto.Gps>();
 
@@ -45,7 +45,7 @@ public class LastPositionUpdator {
             carPostionWriterImpl.updateOnce(carNumber, g.getLatitude(), g.getLongitude());
         };
 
-        var f = scheduledExecutorService.scheduleAtFixedRate(task, 0, 3, java.util.concurrent.TimeUnit.SECONDS);
+        var f = scheduledExecutorService.scheduleAtFixedRate(task, 0, 5, java.util.concurrent.TimeUnit.SECONDS);
 
         ref.set(f);
 
