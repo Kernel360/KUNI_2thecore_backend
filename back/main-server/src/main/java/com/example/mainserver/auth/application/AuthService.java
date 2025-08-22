53cbc4f0739bdace0899dd8bb75dfb949908804b
luckyisjelly <133952441+luckyisjelly@users.noreply.github.com>
2025-08-22 17:38:42 +0900

    Update AuthService.java

--- back/main-server/src/main/java/com/example/mainserver/auth/application/AuthService.java
+++ back/main-server/src/main/java/com/example/mainserver/auth/application/AuthService.java
@@ -33,7 +33,7 @@ public class AuthService {
     private final JwtTokenProvider jwtTokenProvider;
     private final TokenService tokenService;
 
-    private static final long ACCESS_TOKEN_EXPIRE_MINUTES = 10L; // 10분으로 변경
+    private static final long ACCESS_TOKEN_EXPIRE_MINUTES = 1000L; // 1000분으로 변경
     private static final long REFRESH_TOKEN_EXPIRE_DAYS = 7L;
 
     // 로그인
