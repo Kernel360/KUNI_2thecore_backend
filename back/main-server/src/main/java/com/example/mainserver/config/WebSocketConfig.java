109704b2508eb6ffd4ff21ab8968d8b8c8250309
yeojin-g <84260219+yeojin-g@users.noreply.github.com>
2025-10-01 16:32:28 +0900

    feat: 웹소켓 기본 설정 파일 추가

---
+++ back/main-server/src/main/java/com/example/mainserver/config/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package com.example.mainserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    // 클라이언트가 웹소켓 서버에 연결하는데 사용할 엔드포인트 등록
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // STOMP 접속 주소: "/ws
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+    // 메세지 브로커 구성
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // "/location"로 시작하는 주소를 구독하는 클라이언트에게 메세지를 전달하는 브로커
+        registry.enableSimpleBroker("/location");
+
+        // 클라이언트가 서버로 메세지를 보낼 때 사용하는 주소의 prefix
+        // 현재는 사용 X
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
