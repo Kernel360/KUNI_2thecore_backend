bff950b0f216bbca132b1763290b4f4866e858dc
yeojin-g <84260219+yeojin-g@users.noreply.github.com>
2025-10-01 17:58:58 +0900

    feat: dto 코드 형식 수정

--- back/common/src/main/java/com/example/common/dto/LiveLocationDto.java
+++ back/common/src/main/java/com/example/common/dto/LiveLocationDto.java
@@ -1,4 +1,17 @@
 package com.example.common.dto;
 
-public record LiveLocationDto(String carNumber, String latitude, String longitude) {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class LiveLocationDto {
+    // 웹소켓 전달용
+    private String carNumber;
+    private String latitude;
+    private String longitude;
 }
